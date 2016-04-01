e4a5a8765b1d14cf3ac24210cc397b4be712cec2
hahleung <herve.ahleung@gmail.com>
2016-04-01 18:12:30 +0100

    Replace STACK variable by CARDS variable

--- tests.rb
+++ tests.rb
@@ -5,12 +5,12 @@ class TestBoard < Minitest::Test
 
   def test_cards_sum_after_distribution
     (PLAYERS_NUMBER_MAX - 1).times do |x| 
-      distribution = distribute_cards(STACK, x)
+      distribution = distribute_cards(CARDS, x)
       cards_for_players = distribution[0].flatten
       cards_left_in_stack = distribution[1]
 
-      assert_equal (cards_for_players + cards_left_in_stack).length, STACK.length
-      assert_equal (STACK - cards_left_in_stack - cards_for_players), [] 
+      assert_equal (cards_for_players + cards_left_in_stack).length, CARDS.length
+      assert_equal (CARDS - cards_left_in_stack - cards_for_players), [] 
     end
   end
 
