6300bb0ca4a643937446e664283d0c14668ee797
hahleung <herve.ahleung@gmail.com>
2016-04-06 09:52:00 +0100

    Add Name method to Player Class

--- player.rb
+++ player.rb
@@ -4,23 +4,18 @@ require_relative 'stack.rb'
 require_relative 'graveyard.rb'
 
 class Player
-  attr_reader :hand, :tableau, :name
-  def initialize(hand, tableau)
+  attr_reader :name, :hand, :tableau
+  def initialize(name, hand, tableau)
+    @name = name
     @hand = hand
     @tableau = tableau
-  end
-
-  def give_name!
-    puts "Give me your name:"
-    @name = gets.chomp
-    Player.new(@hand, @tableau)
   end
 
   def draw(number, stack)
     new_stack, drawn_cards = stack.draw(number)
     new_hand = @hand.add_cards(drawn_cards)
 
-    [Player.new(new_hand, @tableau), new_stack]
+    [Player.new(@name, new_hand, @tableau), new_stack]
   end
 
   def choose_first_cards(graveyard)
@@ -33,7 +28,7 @@ class Player
     new_hand, discarded_cards = @hand.remove_cards(first, second)
     new_graveyard = graveyard.add_cards(discarded_cards)
 
-    [Player.new(new_hand, @tableau), new_graveyard]
+    [Player.new(@name, new_hand, @tableau), new_graveyard]
   end
 
   def victory_points
--- player_test.rb
+++ player_test.rb
@@ -2,34 +2,34 @@ require 'minitest/autorun'
 require_relative 'player.rb'
 
 class PlayerTest < Minitest::Test
-  def test_give_name
-    puts "For test, write toto."
-    new_player = Player.new([], [])
-    new_player.give_name!
-    assert_equal "toto", new_player.name
-  end
-
   def test_draw
+    card1 = Card.new("card test 1", 1, 0, 2)
+    card2 = Card.new("card test 2", 2, 0, 4)
+    card3 = Card.new("card test 3", 3, 0, 6)
+    cards  = [card1, card2, card3]
+
+    stack = Stack.new(cards)
     hand = Hand.new([])
     tableau = Tableau.new([])
-    stack = Stack.new([1, 2, 3, 4])
-    player = Player.new(hand, tableau)
+    player = Player.new("player", hand, tableau)
 
-    new_player, new_stack = player.draw(4, stack)
+    new_player, new_stack = player.draw(3, stack)
+    cards_hand = new_player.hand.cards
+    organized_hand = (1..3).map{|x| cards_hand.reject{|y| y.id != x} }.flatten
 
-    assert_equal [1, 2, 3, 4], new_player.hand.cards.sort
+    assert_equal cards, organized_hand
   end
 
   def test_victory_points
-    card1 = Card.new("card test 1", 0, 2)
-    card2 = Card.new("card test 2", 0, 4)
-    card3 = Card.new("card test 3", 0, 6)
+    card1 = Card.new("card test 1", 1, 0, 2)
+    card2 = Card.new("card test 2", 2, 0, 4)
+    card3 = Card.new("card test 3", 3, 0, 6)
     set = [card1, card2, card3]
 
     hand = Hand.new([])
     tableau = Tableau.new(set)
     stack = Stack.new([])
-    player = Player.new(hand, tableau)
+    player = Player.new("player", hand, tableau)
 
     assert_equal 12, player.victory_points
   end
@@ -38,7 +38,7 @@ class PlayerTest < Minitest::Test
     hand = Hand.new([1, 2, 3])
     tableau = Tableau.new([])
     graveyard = Graveyard.new([7])
-    player = Player.new(hand, tableau)
+    player = Player.new("player", hand, tableau)
 
     puts "For test, press 1 then 2"
     new_player, new_graveyard = player.choose_first_cards(graveyard)
